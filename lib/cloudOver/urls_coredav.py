0c97c3a7fbffd10c355329ce6541e19cf8bc162f
Maciej Nabozny <mn@mnabozny.pl>
2014-07-18 14:31:01 +0200

    Initial commit with simple package

---
+++ lib/cloudOver/urls_coredav.py
@@ -0,0 +1,27 @@
+"""
+Copyright (c) 2014 Maciej Nabozny
+
+This file is part of OverCluster project.
+
+OverCluster is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^browse/(?P<type>[a-zA-Z]+)/(?P<token>[a-zA-Z0-9]+)/', 'browse.views.browse', name='browse'),
+    url(r'^admin/', include(admin.site.urls)),
+)
